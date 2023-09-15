d5fe6e9c3cff9eb6efa85dff5b54a67dae7be1c4
pedrolopez <pedrolopez@192.168.1.132>
2023-09-15 11:09:39 +0200

    creates, instances and adds Month Class object as part of ArrayList and HashSet Collections for Java & Spring Framework Spring 1 - Task 3, Level 1; demostrations and tests passed

---
+++ src/N1Exe1/ExecuteMonth.java
@@ -0,0 +1,67 @@
+package N1Exe1;
+
+import java.util.*;
+
+public class ExecuteMonth {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Month> monthList = new ArrayList<Month>();
+		
+		monthList.add(new Month ("January"));
+		monthList.add(new Month ("February"));
+		monthList.add(new Month ("March"));
+		monthList.add(new Month ("April"));
+		monthList.add(new Month ("May"));
+		monthList.add(new Month ("June"));
+		monthList.add(new Month ("July"));
+		monthList.add(new Month ("September"));
+		monthList.add(new Month ("October"));
+		monthList.add(new Month ("November"));
+		monthList.add(new Month ("December"));
+		
+		for (Month m : monthList) {
+			System.out.println(m);
+		}
+		
+		monthList.add(7, new Month ("August"));
+				
+		Iterator<Month> iterator = monthList.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+		HashSet<Month> monthList2 = new HashSet<Month>();
+		
+		monthList2.add(new Month ("January"));
+		monthList2.add(new Month ("February"));
+		monthList2.add(new Month ("March"));
+		monthList2.add(new Month ("April"));
+		monthList2.add(new Month ("May"));
+		monthList2.add(new Month ("June"));
+		monthList2.add(new Month ("July"));
+		monthList2.add(new Month ("August"));
+		monthList2.add(new Month ("August")); //Attempt to repete is controlled by Month.hashCode()
+		monthList2.add(new Month ("September"));
+		monthList2.add(new Month ("October"));
+		monthList2.add(new Month ("November"));
+		monthList2.add(new Month ("December"));
+		
+		for (Month m : monthList2) {
+			System.out.println(m + " | Code: " + m.hashCode());
+		}
+				
+		Iterator<Month> iterator2 = monthList2.iterator();
+		while (iterator2.hasNext()) {
+			System.out.println(iterator2.next());
+		}
+		
+		monthList.add(7, new Month ("August")); //Attemp to repete passes
+		
+		for (Month m : monthList) {
+			System.out.println(m);
+		}
+
+	}
+
+}
---
+++ src/N1Exe1/Month.java
@@ -0,0 +1,59 @@
+package N1Exe1;
+
+public class Month {
+	
+	private String monthName;
+
+	public Month(String monthName) {
+		this.monthName = monthName;
+	}
+
+	public String getMonthName() {
+		return monthName;
+	}
+
+	public void setMonthName(String monthName) {
+		this.monthName = monthName;
+	}
+	
+	@Override
+	public int hashCode() { //Comment to pass attempt to repetition
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((monthName == null) ? 0 : monthName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Month))
+			return false;
+		Month other = (Month) obj;
+		if (monthName == null) {
+			if (other.monthName != null)
+				return false;
+		} else if (!monthName.equals(other.monthName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Month: " + monthName;
+	}
+
+}
+
+/*
+ * Crea una clase llamada Month con un atributo "name" 
+ * (que almacenará el nombre del mes del año). Añade 11 objetos Month 
+ * (cada uno con su atributo diferente) en un ArrayList, 
+ * a excepción del objeto con atributo "Agost". 
+ * Después, efectúa la inserción en el sitio que corresponde a este mes y
+ * demuestra que ArrayList mantiene el orden correcto.
+ * Convierte ArrayList del ejercicio anterior en un HashSet 
+ * y asegúrate de que no permite duplicados.
+ * Recorre la lista con un for y con un iterador.
+ */
