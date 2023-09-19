c22353e9aacc2954d4cd00f1d5f211ebcc09786d
pedrolopez <pedrolopez@localhost>
2023-09-19 11:55:13 +0200

    creates preliminar structure for for Java & Spring Framework Spring 1 - Task 3, Level 3

---
+++ src/N3Exe1/Ordering.java
@@ -0,0 +1,11 @@
+package N3Exe1;
+
+import java.util.*;
+
+public class Ordering {
+	
+	TreeSet<Person> personList = new TreeSet<Person>();
+	
+	
+
+}
---
+++ src/N3Exe1/Principal.java
@@ -0,0 +1,10 @@
+package N3Exe1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
